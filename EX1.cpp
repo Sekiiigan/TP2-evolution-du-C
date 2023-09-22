0e1bc65900f3a8fcfcf5b38cc15853fed34cad87
Sekiiigan <vidal.kerian@lyceejulesfil.fr>
2023-09-22 15:53:30 +0200

    first commit

---
+++ EX1.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main()
+{
+    string user ("Lucien");
+    user[0]='J';
+    user[2]='l';
+    cout << "Vous etes : " <<user <<endl;
+    return 0;
+}
