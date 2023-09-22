2e920fdba78d89267db273d6b5397c648cbc3afb
Sekiiigan <vidal.kerian@lyceejulesfil.fr>
2023-09-22 16:25:04 +0200

    EX3

---
+++ EX3.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main()
+{
+    string str("Coucou les copains !");
+    for (char c : str)
+    {
+        cout << c <<' ';
+    }
+    cout << endl;
+    return 0;
+}
